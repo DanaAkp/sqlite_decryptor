e564c3bf9453bc9e4d1355988f9e8dc14be16aae
DanaAkp <aidnagul10@gmail.com>
2021-10-05 21:29:57 +0500

    add new function for adding new table

--- app/static/scripts/add_new_table.js
+++ app/static/scripts/add_new_table.js
@@ -1,16 +1,22 @@
 function add_new_table() {
-    
-    
+    let tbody = document.getElementById('tbody-columns')
+    let rows = tbody.children
+    let columns = []
+    for(let i in rows){
+        let input = rows[i].children
+        let col = {
+            'column_name': input[0],
+            'column_type': input[1],
+            'primary_key': input[2],
+            'nullable': input[3]
+        }
+        columns.push(col)
+    }
+    // columns
 }
 
 function add_column_input() {
     let tbody = document.getElementById('tbody-columns')
-    let child = tbody.textContent
-    // for(let i in child){
-    // //////tbody.childNodes.item(1).childNodes.item()
-    //     console.log(i)
-    //     let buf = child.item(i).children
-    //     // for(let j in buf){console.log(buf.item(j).value)}
-    // }
+
 
 }
